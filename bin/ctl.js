02c06beb1fa48b463383510b54484094a0cac137
Sam Roberts <vieuxtech@gmail.com>
2013-07-03 12:35:21 -0700

    cli connects to server

--- bin/ctl.js
+++ bin/ctl.js
@@ -1,8 +1,27 @@
 #!/usr/bin/env node
 
-version = require('../package.json').version;
-program = require('commander');
+var net = require('net');
+var version = require('../package.json').version;
+var program = require('commander');
 
-program.version(version);
+program
+  .version(version)
+  ;
 
 program.parse(process.argv);
+
+var addr = require('../lib/ctl').ADDR;
+
+var ctl = net.connect(addr, connect)
+  .on('end', function() {
+    console.log('cli - on end');
+  })
+  .on('error', function(er) {
+    console.log('cli - on error', er);
+  });
+
+function connect() {
+  console.log('cli - connect from', this.address(), 'to', this.remoteAddress);
+
+  this.end();
+}
