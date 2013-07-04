0d46e8e39205e1861dcdb60f7d47104c56959fdf
Sam Roberts <vieuxtech@gmail.com>
2013-07-03 21:20:44 -0700

    Add tests for multiple workers, and no workers

--- test/master.js
+++ test/master.js
@@ -33,4 +33,32 @@ describe('master', function() {
       });
     });
   });
+
+  it('should report status 2', function(done) {
+    cluster.fork();
+    cluster.once('fork', function() {
+      cluster.fork();
+      cluster.once('fork', function() {
+        master.request({cmd:'status'}, function(rsp) {
+          assert.equal(rsp.workers.length, 2);
+          var w0 = rsp.workers[0];
+          assert(w0.id);
+          assert(w0.pid > 0);
+
+          var w1 = rsp.workers[1];
+          assert(w1.id);
+          assert(w1.pid > 0);
+          done();
+        });
+      });
+    });
+  });
+
+  it('should report status 0, again', function(done) {
+    master.request({cmd:'status'}, function(rsp) {
+      assert.deepEqual(rsp, {workers:[]});
+      done();
+    });
+  });
+
 });
