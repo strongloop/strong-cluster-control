e528998abb7dc6b675f1d10d325786422d3d0889
Sam Roberts <sam@strongloop.com>
2013-09-26 11:05:00 -0700

    Detect and fail on invalid multiple instantiation
    Using peerDependencies should prevent this situation, but we should fail
    hard and early on invalid usage.

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 // cluster-control:
 
+var assert = require('assert');
 var cluster = require('cluster');
+var VERSION = require('./package.json').version;
+
+if(cluster._strongControlMaster) {
+  assert(
+    cluster._strongControlMaster.VERSION === VERSION,
+    'Multiple versions of strong-cluster-control are being initialized.\n' +
+    'This version ' + VERSION + ' is incompatible with already initialized\n' +
+    'version ' + cluster._strongControlMaster.VERSION + '.\n'
+  );
+  module.exports = cluster._strongControlMaster;
+  return;
+}
 
 if (cluster.isMaster) {
   module.exports = require('./lib/master');
+  module.exports.VERSION = VERSION;
+  cluster._strongControlMaster = module.exports;
 } else {
   // Calling .start() in a worker is a nul op
   exports.start = function (options, callback) {
