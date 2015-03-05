f36a3d9d2734fe266dac60210dc4b67b2df4af3f
Sam Roberts <sam@strongloop.com>
2015-03-05 11:22:42 -0800

    lint: add eslint and jscs support

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var assert = require('assert');
 var cluster = require('cluster');
 var VERSION = require('./package.json').version;
 
-if(cluster._strongControlMaster) {
+if (cluster._strongControlMaster) {
   assert(
     cluster._strongControlMaster.VERSION === VERSION,
     'Multiple versions of strong-cluster-control are being initialized.\n' +
@@ -22,24 +22,24 @@ if (cluster.isMaster) {
 } else {
   exports = module.exports = new (require('events').EventEmitter);
   // Calling .start() in a worker is a nul op
-  exports.start = function (options, callback) {
+  exports.start = function(options, callback) {
     // both options and callback are optional, adjust position based on type
     // XXX cut-n-paste from lib/master, is it possible to factor out, maybe
     // into a function that modifies arguments?
-    if(typeof callback === 'undefined') {
-      if(typeof options === 'function') {
+    if (typeof callback === 'undefined') {
+      if (typeof options === 'function') {
         callback = options;
         options = undefined;
       }
     }
 
-    if(callback) {
+    if (callback) {
       process.nextTick(callback);
     }
     return this;
   };
   exports.stop = function(callback) {
-    if(callback) {
+    if (callback) {
       process.nextTick(callback);
     }
     return this;
