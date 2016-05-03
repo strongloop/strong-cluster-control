2ae081d08a891af6b60e7bfa14768678321ac018
Ryan Graham <r.m.graham@gmail.com>
2016-05-03 16:40:25 -0700

    update/insert copyright notices

--- test/test-restart-failed-5.js
+++ test/test-restart-failed-5.js
@@ -1,2 +1,7 @@
+// Copyright IBM Corp. 2015. All Rights Reserved.
+// Node module: strong-cluster-control
+// This file is licensed under the Artistic License 2.0.
+// License text available at https://opensource.org/licenses/Artistic-2.0
+
 global.SIZE = 5;
 require('./restart-failed');
